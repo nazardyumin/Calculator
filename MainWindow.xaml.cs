cebe5be78c9345deb71857e10c3d9a5a44cab8c9
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2022-09-01 11:46:17 +0500

    Add some buttons and fontsizing!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,33 +23,163 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+            TextDown.Text += "0";
         }
 
         private void ButtonCE_Click(object sender, RoutedEventArgs e)
         {
-            TextDown.Text="";
+            TextDown.Text = "0";
+            TextDown.FontSize =45;
         }
 
         private void ButtonC_Click(object sender, RoutedEventArgs e)
         {
-            TextUp.Text += "7";
+            TextDown.Text += "0";
+            TextDown.FontSize = 45;
         }
 
         private void Button7_Click(object sender, RoutedEventArgs e)
         {
-            if (TextDown.Text.Length%4==0)
+            if (TextDown.Text.Length == 11)
             {
-                TextDown.Text += " ";
+                TextDown.FontSize -= 9;
             }
-            if (TextDown.Text.Length == 14)
+            if (TextDown.Text.Length == 15)
             {
-                TextDown.FontSize -= 10;
+                TextDown.FontSize -= 7;
             }
-            if (TextDown.Text.Length == 20)
+            if (TextDown.Text.Length == 19)
             {
-                TextDown.FontSize -= 10;
+                TextDown.FontSize -= 5;
             }
-            TextDown.Text += "7";
+            if (TextDown.Text.Length == 23)
+            {
+                TextDown.FontSize -= 3.5;
+            }
+            if (TextDown.Text.Length >= 27)
+            {
+                TextDown.TextWrapping = TextWrapping.WrapWithOverflow;
+            }
+            if (TextDown.Text.Length == 54)
+            {
+                TextDown.FontSize -= 3;
+            }
+            if (TextDown.Text.Length == 95)
+            {
+                TextDown.FontSize -= 4;
+            }
+            if (TextDown.Text=="0")
+            {
+                TextDown.Text = "7";
+            }
+            else
+            {
+                if (TextDown.Text.Length == 3)
+                {
+                    TextDown.Text += " ";
+                }
+                if (TextDown.Text.Length >= 7 && TextDown.Text[^1] != ' ' && TextDown.Text[^2] != ' ' && TextDown.Text[^3] != ' ')
+                {
+                    TextDown.Text += " ";
+                }
+                TextDown.Text += "7";
+            }     
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextDown.Text.Length == 11)
+            {
+                TextDown.FontSize -= 9;
+            }
+            if (TextDown.Text.Length == 15)
+            {
+                TextDown.FontSize -= 7;
+            }
+            if (TextDown.Text.Length == 19)
+            {
+                TextDown.FontSize -= 5;
+            }
+            if (TextDown.Text.Length == 23)
+            {
+                TextDown.FontSize -= 3.5;
+            }
+            if (TextDown.Text.Length >= 27)
+            {
+                TextDown.TextWrapping = TextWrapping.WrapWithOverflow;
+            }
+            if (TextDown.Text.Length == 54)
+            {
+                TextDown.FontSize -= 3;
+            }
+            if (TextDown.Text.Length == 95)
+            {
+                TextDown.FontSize -= 4;
+            }
+            if (TextDown.Text == "0")
+            {
+                TextDown.Text = "8";
+            }
+            else
+            {
+                if (TextDown.Text.Length == 3)
+                {
+                    TextDown.Text += " ";
+                }
+                if (TextDown.Text.Length >= 7 && TextDown.Text[^1] != ' ' && TextDown.Text[^2] != ' ' && TextDown.Text[^3] != ' ')
+                {
+                    TextDown.Text += " ";
+                }
+                TextDown.Text += "8";
+            }
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {           
+            if (TextDown.Text.Length == 11)
+            {
+                TextDown.FontSize -= 9;
+            }
+            if (TextDown.Text.Length == 15)
+            {
+                TextDown.FontSize -= 7;
+            }
+            if (TextDown.Text.Length == 19)
+            {
+                TextDown.FontSize -= 5;
+            }
+            if (TextDown.Text.Length == 23)
+            {
+                TextDown.FontSize -= 3.5;
+            }
+            if (TextDown.Text.Length >= 27)
+            {
+                TextDown.TextWrapping = TextWrapping.WrapWithOverflow;
+            }
+            if (TextDown.Text.Length == 54)
+            {
+                TextDown.FontSize -= 3;
+            }
+            if (TextDown.Text.Length == 95)
+            {
+                TextDown.FontSize -= 4;
+            }
+            if (TextDown.Text == "0")
+            {
+                TextDown.Text = "9";
+            }
+            else
+            {
+                if (TextDown.Text.Length == 3)
+                {
+                    TextDown.Text += " ";
+                }
+                if (TextDown.Text.Length >= 7 && TextDown.Text[^1]!=' '&& TextDown.Text[^2] != ' ' && TextDown.Text[^3] != ' ')
+                {
+                    TextDown.Text += " ";
+                }
+                TextDown.Text += "9";
+            }
         }
     }
 }
